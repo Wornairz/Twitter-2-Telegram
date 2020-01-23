11fcc920f015c35bfeb55a0f7d1fb754b591bc56
Wornairz <wornairz@users.noreply.github.com>
2020-01-23 18:30:04 +0100

    Implemented loop requests to check for updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from requests_oauthlib import OAuth1
 import yaml
 import time
+import json
 
 def load_settings():
     with open('auth.yaml') as yaml_auth:
@@ -15,15 +16,29 @@ def load_settings():
         oauth_token = auth_map["oauth_token"]
         oauth_token_secret = auth_map["oauth_token_secret"]
 
-def main(): 
+def main():
     load_settings()
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    querystring = {"screen_name":"azione_it","tweet_mode":"extended","exclude_replies":"true","include_rts":"false"}
     auth1 = OAuth1(oauth_app_key, oauth_app_secret, oauth_token, oauth_token_secret)
-
+    querystring = {"screen_name":"azione_it","tweet_mode":"extended","exclude_replies":"true","include_rts":"true","count":1}
     response = requests.get(url, params=querystring, auth=auth1)
+    last_tweet = (json.loads(response.text))[0]["id"]
+    print(last_tweet)
+    time.sleep(300)
 
-    print(response.text)
+    while(True):
+        try:
+            querystring = {"screen_name":"azione_it","tweet_mode":"extended","exclude_replies":"true","include_rts":"true", "since_id": last_tweet}
+            auth1 = OAuth1(oauth_app_key, oauth_app_secret, oauth_token, oauth_token_secret)
+            response = requests.get(url, params=querystring, auth=auth1)
+            response_json = json.loads(response.text)
+            if len(response_json) is not 0:
+                print("----------------")
+                print(response_json)
+                last_tweet = response_json[0]["id"]
+        except Exception as e:
+            print("Errore")
+        time.sleep(300)
 
 if __name__ == '__main__':
     main()
